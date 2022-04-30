3eec5b1e5204fb271b8c714a38b9d3c444ddd29c
Oleg Valter <o.a.philippov@gmail.com>
2022-05-01 02:02:00 +0300

    1.2.0

--- dist/headers.js
+++ dist/headers.js
@@ -51,5 +51,5 @@
 // @run-at          document-start
 // @source          git+https://github.com/userscripters/dupe-timeline-lists.git
 // @supportURL      https://github.com/userscripters/dupe-timeline-lists/issues
-// @version         1.1.0
+// @version         1.2.0
 // ==/UserScript==
