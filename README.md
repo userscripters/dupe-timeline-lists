763ec993e1fa91f758653c62f1e28d8033ef6cc1
Oleg Valter <o.a.philippov@gmail.com>
2022-09-01 02:29:37 +0300

    updated README generator script to have a note about the breaking change

--- README.md
+++ README.md
@@ -11,6 +11,10 @@
 The native post timeline displays "duplicate lists edited" event as a barely readable blob of links with excessive verbosity ("duplicate list changed from [list of links] to [list of links]"). This userscript diffs the changes and displays them as easy to visually parse lists of items.
 
 As of v1.2.0, also fixes duplicate list edit entries on /revisions pages.
+
+As of v3.0.0 uses its own storage for optional configuration (available by installing a [Userscript Configurer][1] peer dependency)
+
+  [1]: https://github.com/userscripters/userscripts-configurer/raw/master/dist/modern/index.user.js
 # Screenshots
 ![https://i.stack.imgur.com/kgTUf.png](https://i.stack.imgur.com/kgTUf.png)
 
--- scripts/readme.sh
+++ scripts/readme.sh
@@ -2,4 +2,8 @@ generate-readme \
     --screenshot "https://i.stack.imgur.com/kgTUf.png" \
     --about "The native post timeline displays \"duplicate lists edited\" event as a barely readable blob of links with excessive verbosity (\"duplicate list changed from [list of links] to [list of links]\"). This userscript diffs the changes and displays them as easy to visually parse lists of items.
 
-As of v1.2.0, also fixes duplicate list edit entries on /revisions pages."
+As of v1.2.0, also fixes duplicate list edit entries on /revisions pages.
+
+As of v3.0.0 uses its own storage for optional configuration (available by installing a [Userscript Configurer][1] peer dependency)
+
+  [1]: https://github.com/userscripters/userscripts-configurer/raw/master/dist/modern/index.user.js"
